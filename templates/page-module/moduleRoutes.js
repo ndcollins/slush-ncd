faaea5efa67d3c7b59e5ba19e8eabc044c50f1f1
Nick Collins <ndcollins@nc-design.net>
2015-08-12 14:51:27 -0500

    Updated to version 0.4.0

--- templates/page-module/moduleRoutes.js
+++ templates/page-module/moduleRoutes.js
@@ -4,12 +4,12 @@ function <%= moduleName %>Routes($stateProvider) {
 
     var home = {
         name: '<%= nameLower %>', // state name
-        url: '/', // url path that activates this state
+        url: '/<%= moduleNameDashed%>', // url path that activates this state
         template: '<div <%= moduleNameDashed%> ></div>', // generate the Directive "homeView" - when calling the directive in HTML, the name must not be camelCased
         data: {
             moduleClasses: 'page', // assign a module class to the <body> tag
-            pageClasses: 'home', // assign a page-specific class to the <body> tag
-            pageTitle: 'Home', // set the title in the <head> section of the index.html file
+            pageClasses: '<%= moduleNameDashed%>', // assign a page-specific class to the <body> tag
+            pageTitle: '<%= module %>', // set the title in the <head> section of the index.html file
             pageDescription: 'Meta Description goes here' // meta description in <head>
         }
     };
@@ -18,5 +18,5 @@ function <%= moduleName %>Routes($stateProvider) {
 
 }
 
-homeRoutes.$inject = ['$stateProvider'];
-module.exports = homeRoutes;
+<%= moduleName %>Routes.$inject = ['$stateProvider'];
+module.exports = <%= moduleName %>Routes;
